0f2c728878f400d7486c49a5dfb9e67058587e91
palomabareli <paloma.bareli@gmail.com>
2018-09-08 16:45:54 -0300

    Third part of day 08/09/2018

--- 01_Class_20180901/Exercise_3.py
+++ 01_Class_20180901/Exercise_3.py
@@ -8,12 +8,30 @@ Calcular a soma das diagonais da matriz
 [3][4][5]
 [6][7][8]
 '''
-#Three lists with three itens
-countList, countItem = 3, 3
 
-Matrix = [[0 for x in range(w)] for y in range(h)] 
 
-print(list_number) 
+lineNumber = 4   #não inclusivo    
+columnNumber = 4 #não inclusivo  
 
-print('First diagonal: ')
-print('Second diagonal: ')
+#matrix = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+matrix = [[0, 0, 1, 2], [2, 2, 3, 4], [4, 4, 5, 6], [6, 6, 7, 8]]
+
+sumFisrtDiagonal, sumSecondDiagonal = 0, 0
+auxColumn, auxLine = 0, 0
+
+for line in range(lineNumber):
+    for column in range(columnNumber):
+        print('line = {0}, column = {1}, matrix[{0}][{1}] = {2}'.\
+              format(line,column,matrix[line][column]))
+
+for line in range(lineNumber):
+    for column in range(columnNumber):
+        
+        if (line == column):
+            sumFisrtDiagonal += int(matrix[line][column])
+            #print(matrix[line][column])   
+   		
+sumSecondDiagonal = sumFisrtDiagonal
+		
+print('First diagonal: {0}'.format(str(sumFisrtDiagonal)))
+print('Second diagonal: {0}'.format(str(sumSecondDiagonal)))
